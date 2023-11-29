152cff32d5ac061402095d79129a02cb8fd3daf9
Hasith Perera <12579141+hasithperera@users.noreply.github.com>
2023-11-29 00:28:26 -0500

    code updates: 2023-11-29 00:28:26

--- src/rfd900_sd.py
+++ src/rfd900_sd.py
@@ -16,25 +16,49 @@ file = '../Data/RFD900/PAYLOAD.CSV.5';
 df = pd.read_csv(file)
 
 
-cols = ['Analog External','Analog Internal','Digital Internal','Digital External','Accel A', 'Accel Y', 'Accel Z']
+cols = ['Analog External','Analog Internal','Digital Internal','Digital External',
+        'Accel A', 'Accel Y', 'Accel Z',
+        'Latitude', 'Longitude',
+        'Battery',
+        ]
 
-#for col in cols[0:4]:
-#    df[col] = df[col].astype(float)
-#    plt.plot(df[col])
-#    
-#plt.legend(cols)
-#plt.ylabel(r'$c$')
-#plt.xlabel(r'$time (s)$')
+print(df.keys())
 
-plt.figure()
-
-for col in cols[4:]:
+# change data type 
+for col in cols:
     df[col] = df[col].astype(float)
-    plt.plot(df[col])
+    
     
 
+plt.figure(1)
+for col in cols[0:4]:
+    plt.plot(df[col])
+    
+    
+plt.legend(cols)
+plt.ylabel(r'$Temp (C^\degree)$')
+plt.xlabel(r'$time (s)$')
 
 
+plt.figure(2)
+plt.title('Acc x,y,z')
+for col in cols[4:7]:
+    plt.plot(df[col])
+    
+plt.xlabel('time (s)')
+    
+    
+plt.figure(3)
+plt.title('lon/lat')
 
-#
-#
+for col in cols[7:]:
+    plt.plot(df[col])
+    
+plt.xlabel('time (s)')
+    
+    
+# battery decay curve
+plt.figure(4)
+plt.plot(df['Battery'],'.')
+plt.ylabel('Voltage (V)')
+plt.xlabel('time (s)')
